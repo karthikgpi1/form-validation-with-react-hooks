3755db05c43d1c38ef89531f16838dcafe21536a
karthikgpi1 <karthikgopi024@gmail.com>
2022-09-02 12:18:46 +0530

    first commit

---
+++ src/components/SignUpInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignUpInfo() {
+  return (
+    <div className="sign-up-container">
+       <input type="email" placeholder="Email.."/>
+       <input type="text" placeholder="password.."/>
+       <input type="text" placeholder="Confirm password.."/>
+    </div>
+  )
+}
+
+export default SignUpInfo;
---
+++ src/components/form.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+import SignUpInfo from "./SignUpInfo";
+import PersonalInfo from "./PersonalInfo";
+import OtherInfo from "./OtherInfo";
+
+function Form() {
+    const [page, setPage] = useState(0);
+
+    const FormTitles = ["Sign up", "Personal Info", "other"];
+
+    const PageDisplay =() => {
+        if (page === 0) {
+            return <SignUpInfo />;
+        } else if ( page ===1) {
+            return <PersonalInfo />;
+        } else {
+            return <OtherInfo/>;
+        }
+    };
+
+  return (
+    <div className="form">
+         <div className="progressbar"></div>
+         <div className="form-container">
+            <div className="header">
+                <h1>{FormTitles[page]}</h1>
+            </div>
+            <div className="body">
+                {PageDisplay()}
+            </div>
+            <div className="footer">
+                <button 
+                    disabled={page == 0}
+                    onClick={() => {
+                    setPage((currPage) => currPage - 1);}}>Prev</button>
+                <button
+                    disabled={page == FormTitles.length -1}
+                    onClick={() => {
+                    setPage((currPage) => currPage + 1);}}>Next</button>
+            </div>
+         </div>
+    </div>
+  )
+}
+
+export default Form;
